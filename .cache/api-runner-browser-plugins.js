1e1bcc092f7e31f3fe4174c5422d5e3dac3ac944
Christie Chang <christieychang@gmail.com>
2019-07-19 01:14:46 -0400

    replacing old files with gatsby webapp woohoo

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/christiechang/Documents/GitHub/christieychang.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import TempLayout from "../components/temp_layout.js"
+import SEO from "../components/seo"
+
+
+
+const IndexPage = () => (
+
+  <TempLayout>
+    <h1>Christie Chang</h1><br />
+    <p>Hi there! This site is undergoing changes.<br />It'll be up and running again soon.</p>
+    <p>In the meantime:</p>
+  </TempLayout>
+)
+
+export default IndexPage
